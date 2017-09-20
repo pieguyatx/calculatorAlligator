f2943e51f4a5cb8b524e482da770db2a0174cb87
Pius Wong <pius@pioslabs.com>
2017-09-20 16:52:19 -0500

    Control SVG size w/ CSS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-  // initalize images
+  // initialize common variables
 
-  // calculate
+  // Read in button presses
+  // If it's the first button press after clearing... (state = new, display = 0)
+    // assume dislpayed number is 0
+  // else if button pressed while a number or decimal is displayed (state = number)
+    // If button is a non-zero number, put in display on right side of number
+    // If button is an operator, assume first number is zero (0)
+      // then go to the operator function
+    // If button is (clear), run the CLEAR function
+    // If button is the (decimal), add decimal to right side (0.)
+    // If button is the (equals) or (sign), do nothing
+  // else if button pressed while operator after a number is displayed (state = operator)
+
+  // calculate appropriately
 
   // display result
 
